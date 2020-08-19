8c88dcec1563666c790e1b68dec304365620b145
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-19 12:57:53 +0200

    fix

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,41 +2,76 @@ const config = require("config");
 const path = require("path");
 
 module.exports = {
+  
   development: {
-    client: "sqlite3",
+    client: 'mysql',
     connection: {
-      filename: path.resolve(__dirname) + "/src/db/dev.sqlite3"
-    },
-    // seeds: {
-    //   directory: path.resolve(__dirname) + "/src/db/seeds/development"
-    // },
-    migrations: {
-      tableName: "migrations",
-      directory: path.resolve(__dirname) + "/src/db/migrations"
-    },
-    useNullAsDefault: true
+        host: '172.18.0.2',
+        user: 'melo',
+        password: 'smelo',
+        database: 'aviot'
+    }
   },
+  
   staging: {
-    client: "sqlite3",
+    client: 'mysql',
     connection: {
-      filename: path.resolve(__dirname) + "/src/db/staging.sqlite3"
-    },
-    migrations: {
-      tableName: "migrations",
-      directory: path.resolve(__dirname) + "/src/db/migrations"
-    },
-    useNullAsDefault: true
+        host: '172.18.0.2',
+        user: 'melo',
+        password: 'smelo',
+        database: 'aviot'
+    }
   },
 
   production: {
-    client: "sqlite3",
+    client: 'mysql',
     connection: {
-      filename: path.resolve(__dirname) + "/src/db/production.sqlite3"
-    },
-    migrations: {
-      tableName: "migrations",
-      directory: path.resolve(__dirname) + "/src/db/migrations"
-    },
-    useNullAsDefault: true
+        host: '172.18.0.2',
+        user: 'melo',
+        password: 'smelo',
+        database: 'aviot'
+    }
   }
 };
+
+// module.exports = {
+  
+//   development: {
+//     client: "sqlite3",
+//     connection: {
+//       filename: path.resolve(__dirname) + "/src/db/dev.sqlite3"
+//     },
+//     // seeds: {
+//     //   directory: path.resolve(__dirname) + "/src/db/seeds/development"
+//     // },
+//     migrations: {
+//       tableName: "migrations",
+//       directory: path.resolve(__dirname) + "/src/db/migrations"
+//     },
+//     useNullAsDefault: true
+//   },
+  
+//   staging: {
+//     client: "sqlite3",
+//     connection: {
+//       filename: path.resolve(__dirname) + "/src/db/staging.sqlite3"
+//     },
+//     migrations: {
+//       tableName: "migrations",
+//       directory: path.resolve(__dirname) + "/src/db/migrations"
+//     },
+//     useNullAsDefault: true
+//   },
+
+//   production: {
+//     client: "sqlite3",
+//     connection: {
+//       filename: path.resolve(__dirname) + "/src/db/production.sqlite3"
+//     },
+//     migrations: {
+//       tableName: "migrations",
+//       directory: path.resolve(__dirname) + "/src/db/migrations"
+//     },
+//     useNullAsDefault: true
+//   }
+// };
