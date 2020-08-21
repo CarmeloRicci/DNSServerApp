b1c3b070fbca3feb8520ea56a26645a6afee6f2a
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-21 20:55:39 +0200

    ok

--- backend/src/services/hostServices.ts
+++ backend/src/services/hostServices.ts
@@ -18,35 +18,41 @@ export default class DnsService {
 
   }
 
-    async FindIpInToHostsFile(device: IHostDevice) {
-    var preserveFormatting = false
+  async FindIpInToHostsFile(device: IHostDevice) {
+    
+    const preserveFormatting = false
 
+    let temp:any
     await hostile.get(preserveFormatting, function (err: any, lines: any) {
       if (err) {
         console.error(err.message)
-      } else {
-        for (let i = 0; i < lines.length; i++) {
-          console.log(i + " -> " + lines[i])
-          if (device.ip == lines[i][0]) {
-            if (device.host != lines[i][1]){
-              this.UpdateRecordHostsFile(device,lines[i][1])
-            }            
-          }else{
-            this.InsertRecordHostsFile(device)
+      }
+      temp = lines
+      lines.forEach(function (line: any) {
+        console.log(line) // [IP, Host]
+      })
+    })
+
+    for (let i = 0; i < temp.length; i++) {
+          console.log(i + " -> " + temp[i])
+          if (device.ip == temp[i][0]) {
+            if (device.host != temp[i][1]) {
+              await this.UpdateRecordHostsFile(device, temp[i][1])
+            }
+          } else {
+            await this.InsertRecordHostsFile(device)
           }
 
         }
       }
-    })
-  }
-  
+
 
   async RawDataToArrayDevices(raw: any) {
     let temp: IHostDevice[] = raw
     return temp
   }
 
-  async UpdateRecordHostsFile(device: IHostDevice, oldhostname: string){
+  async UpdateRecordHostsFile(device: IHostDevice, oldhostname: string) {
     await hostile.remove(device.ip, oldhostname, function (err: any) {
       if (err) {
         console.error(err)
@@ -57,7 +63,7 @@ export default class DnsService {
     await this.InsertRecordHostsFile(device)
   }
 
-  async InsertRecordHostsFile(device: IHostDevice){
+  async InsertRecordHostsFile(device: IHostDevice) {
     await hostile.set(device.ip, device.host, function (err: any) {
       if (err) {
         console.error(err)
