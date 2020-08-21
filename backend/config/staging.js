80746e5ffd5001f88345a84562a56a4b05c2aeb9
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-21 19:24:04 +0200

    ok

--- backend/config/staging.js
+++ backend/config/staging.js
@@ -7,11 +7,16 @@ module.exports = {
     interface: "edge0",
     entry_interface: "iface"
   },
-  watcher: {
-    path_to_watch: "/proc/net/arp",
-    leases_path: "/var/lib/misc/dnsmasq.leases"
+  watcher_apr: {
+    path_to_watch: "/proc/net/arp"
+  },
+  watcher_leases: {
+    path_to_watch: "/var/lib/misc/dnsmasq.leases"
   },
   general: {
-    ipDnsServer: "10.10.0.1"
+    ipDnsServer: "10.10.0.1",
+    tenant_id: process.env.TENANTID,
+    //tenant_id: "Agri01",
+    ipBackend: "172.19.0.4"
   }
 };
