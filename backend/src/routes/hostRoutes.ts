828b486b6bb530af306a65f9b77e0b9707a83fb5
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-29 16:48:28 +0200

    ok

--- backend/src/routes/hostRoutes.ts
+++ backend/src/routes/hostRoutes.ts
@@ -8,15 +8,15 @@ const delay = require('delay');
 import HostService from '../services/hostServices';
 const hostService = new HostService();
 
-router.post('/refresh_host', async (req, res) => {
+router.post('/refresh', async (req, res) => {
     const body = req.body;
     var ip = req.connection.remoteAddress.split(":")[((req.connection.remoteAddress.split(":")).length)-1]
     try {
         const params = body && body.params ? body.params : null;
         console.log("hostRoutes received("+ip+"): ","PARAMS", params);
-        if (params) {
+        //if (params) {
             await hostService.NewRulesForHostFile(params);
-        }
+        //}
         res.status(HttpStatus.OK).send();
     } catch (error) {
         res.status(HttpStatus.OK).send(error);
